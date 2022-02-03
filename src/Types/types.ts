8196d56d32ab70ff0fa02cebd34f7872fc40e7ff
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-02-03 14:29:07 +0100

    types

---
+++ src/Types/types.ts
@@ -0,0 +1,26 @@
+// MARK: - Dish
+export interface IDish {
+     id: number
+     name:string 
+     image:string 
+     category:string 
+     label: string
+     price:string
+     dishDescription: string
+     comments: string
+
+    // enum CodingKeys: String, CodingKey {
+    //     case id, name, image, category, label, price
+    //     case dishDescription = "description"
+    //     case comments
+    // }
+}
+
+// MARK: - Comment
+export interface IComment {
+    id:number 
+    rating: number
+    comment:string
+    author: string
+    date: string
+}
