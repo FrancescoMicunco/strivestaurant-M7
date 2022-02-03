26c971582da1780623bd83297c405040a6ae9dc3
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2022-02-03 16:21:09 +0100

    details

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -3,9 +3,10 @@ import { useParams } from 'react-router-dom'
 import dishes from '../data/menu.json'
 import DishComments from './DishComments'
 import { Col, Row, Container } from 'react-bootstrap'
+import { IDish } from '../Types/types'
 
 const Details = () => {
-  const [pasta, setPasta] = useState(undefined)
+  const [pasta, setPasta] = useState<IDish>()
 
   const params = useParams()
 
